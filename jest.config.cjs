69bccc0e0bc32388cc8605416263b017f1394e89
DaoNgocAnh25092004 <161846189+DaoNgocAnh25092004@users.noreply.github.com>
2026-09-26 14:17:48 +0700

    build(config): configure order path aliases

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,11 +1,12 @@
 // Cấu hình Jest cho unit test của Order Service.
 module.exports = {
-  moduleFileExtensions: ["js", "json", "ts"],
-  rootDir: ".",
-  testRegex: ".*\\.spec\\.ts$",
-  transform: { "^.+\\.(t|j)s$": "ts-jest" },
-  moduleNameMapper: {
-    "^@common/(.*)$": "<rootDir>/../../packages/common/$1",
-  },
-  testEnvironment: "node",
+    moduleFileExtensions: ['js', 'json', 'ts'],
+    rootDir: '.',
+    testRegex: '.*\\.spec\\.ts$',
+    transform: { '^.+\\.(t|j)s$': 'ts-jest' },
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+        '^@common/(.*)$': '<rootDir>/../../packages/common/$1',
+    },
+    testEnvironment: 'node',
 };
